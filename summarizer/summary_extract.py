0b1c6e1f1a450955a3b7abee9be386697ae207bc
Bivu Raj Dhungana <brdhunga@gmail.com>
2017-02-03 14:08:20 -0600

    keywords and title_score functions test passed

--- summarizer/summary_extract.py
+++ summarizer/summary_extract.py
@@ -1,4 +1,9 @@
 from re import split as regex_split, sub as regex_sub, UNICODE as REGEX_UNICODE
+from collections import Counter
+
+from .utils import STOPWORDS
+
+MIN_KEYWORD_LENGTH = 15
 
 
 def split_sentences(text):
@@ -12,3 +17,42 @@ def split_sentences(text):
     return s_iter
 
 
+def split_words(text):
+    #split a string into array of words
+    try:
+        text = regex_sub(r'[^\w ]', '', text, flags=REGEX_UNICODE)  # strip special chars
+        return [x.strip('.').lower() for x in text.split()]
+    except TypeError:
+        print("Error while splitting words")
+        return None
+
+
+def keywords(text):
+    """
+    get the top 15 words
+    """
+    text = split_words(text)
+    num_of_words = len(text) 
+    freq = Counter(x for x in text if x not in STOPWORDS)
+
+    min_size = min(MIN_KEYWORD_LENGTH, len(freq)) 
+    keywords_ = {x: y for x, y in freq.most_common(min_size)}
+
+    for k in keywords_:
+        article_score = keywords_[k]*1.0 / num_of_words
+        keywords_[k] = article_score * 1.5 + 1
+
+    return keywords_
+
+
+def title_score(title, sentence):
+    title = [x for x in title if x not in STOPWORDS]
+    count = 0.0
+    for word in sentence:
+        if (word not in STOPWORDS and word in title):
+            count += 1.0
+            
+    if len(title) == 0:
+        return 0.0
+        
+    return count/len(title)
--- summarizer/test_dummary_extract.py
+++ summarizer/test_dummary_extract.py
@@ -1,10 +1,32 @@
-from .summary_extract import split_sentences
+from .summary_extract import (split_sentences, split_words, 
+                                keywords, title_score)
 
 
-test_sentence='''
+test_paragraph='''
 Sentence 1. Sentence's 2 type.  I'm another sentence.
 '''
 
-def test_sentence_are_splitted():
-    assert len(split_sentences(test_sentence) ) == 3
+def test_paragraph_is_splitted():
+    assert len(split_sentences(test_paragraph) ) == 3
 
+
+test_sentece = "This is a sentence."
+
+def test_sentence_is_splitted():
+    assert len(split_words(test_sentece)) == 4
+
+
+test_sentence_for_keyword = "Man man dog goes to dog dog"
+
+def test_keywords_extracted_from_sentence():
+    keywords_ = keywords(test_sentence_for_keyword)
+    assert round(keywords_["man"], 3) == round(2.0/7*1.5+1, 3)
+
+
+title = ["this", "is", "title"]
+sentence = ["new", "way", "to", "go"]
+alt_sentence = ["new", "way", "to", "go", "title", "title"]
+
+def test_title_score():
+    assert title_score(title, sentence) == 0.0
+    assert title_score(title, alt_sentence) == 2.0
