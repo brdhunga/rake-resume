f5cca94155d5e58fa155760c4162dd6fb6e420d1
Bivu Raj Dhungana <brdhunga@gmail.com>
2017-02-03 21:56:57 -0600

    common words between summary and keywords display as output

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from collections import OrderedDict
 from flask import Flask, render_template, request, redirect, url_for
 
 from rake import Rake, text
+from summarizer import summarize
 
 
 app = Flask(__name__)
@@ -16,19 +17,57 @@ def turn_tuple_to_dict(tuples):
 	return dict_
 
 
+def flatten_values(input_):
+    '''
+    finds single words in keys of a dictionary
+    e.g. dict_ = {"man in moon": 3, "what's this: 5", "bivu": 4}
+    ["what's", 'this', 'bivu', 'man', 'in', 'moon']
+    '''
+    if type(input_) == dict:
+        list_to_parse = input_.keys()
+    else:
+        list_to_parse = input_
+    final_word_list = []
+    for each_ in list_to_parse:
+        final_word_list += list(map(lambda x: x.strip(), each_.split(" ")))
+    return final_word_list
+
+
+
+def get_common_words(keywords, sentences):
+    '''
+    get words that are comming using both rake and NLTK
+    keywords is a dictinary 
+    sentences is a list of sentences
+    '''
+    words_from_keywords = flatten_values(keywords)
+    words_from_summaries = flatten_values(sentences)
+    return [element for element in words_from_keywords if element in words_from_summaries]
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
+        if request.method == "POST":
 
-        job_description = request.form["description"]
-        rake = Rake("all_stop_words.txt")
-        keyword_tuples = rake.run(job_description)
-        keyword_dict = turn_tuple_to_dict(keyword_tuples)        
-        return render_template("results.html", keywords=keyword_dict)
+                job_description = request.form["description"]
+                job_title = request.form["title"]
+                
+                rake = Rake("all_stop_words.txt")
+                keyword_tuples = rake.run(job_description)
+                keyword_dict = turn_tuple_to_dict(keyword_tuples)
+                
+                important_sentences = summarize(job_title, job_description) 
+                
+                common_words = get_common_words(keyword_dict, important_sentences)                
+               
+                return render_template("results.html", 
+                                    keywords=keyword_dict, 
+                                    summaries=important_sentences,
+                                    common_words = common_words)
 
-    return render_template('index.html')
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
